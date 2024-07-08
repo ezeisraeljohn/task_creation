57a9687ac2842e4a7df7031801c715dd8482be4a
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-08 21:04:57 +0100

    feat: Add URL configuration for task_creation_app API
    This commit adds the URL configuration file for the task_creation_app API. The file defines the URL paths for various API endpoints, including tasks and users. It also includes the necessary imports and view functions for handling these endpoints.
    
    The addition of this file is a crucial step in setting up the API routing and enabling access to the task and user resources.

---
+++ task_creation_app/api/urls.py
@@ -0,0 +1,34 @@
+""" URL Configuration for the task_creation_app API 
+    
+        Args:
+                path (str): The URL path
+                include (str): The URL include
+        
+        
+                
+        Raises:
+                No exceptions
+"""
+
+from django.urls import path, include
+from .views import TaskViewSet, UserViewSet
+from .views import api_root
+
+task_list = TaskViewSet.as_view({"get": "list", "post": "create"})
+task_detail = TaskViewSet.as_view(
+    {"get": "retrieve", "put": "update", "delete": "destroy"}
+)
+
+user_list = UserViewSet.as_view({"get": "list", "post": "create"})
+user_detail = UserViewSet.as_view(
+    {"get": "retrieve", "put": "update", "delete": "destroy"}
+)
+
+
+urlpatterns = [
+    path("", api_root, name="api-root"),
+    path("tasks/", task_list, name="task-list"),
+    path("tasks/<int:pk>/", task_detail, name="task-detail"),
+    path("users/", user_list, name="user-list"),
+    path("users/<int:pk>/", user_detail, name="user-detail"),
+]
