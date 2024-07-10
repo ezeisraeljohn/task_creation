33f1c38e32eeec9f99d071d737ebf0d7738229d0
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-10 14:52:17 +0100

    chore: Add post_save signal to create auth token for new users
    This commit adds a post_save signal to the models.py file in order to automatically create an authentication token for new users. The signal is triggered when a new user is created and uses the rest_framework.authtoken app to generate a token associated with the user. This functionality ensures that new users have a valid token for authentication purposes.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change.

--- task_creation_app/models.py
+++ task_creation_app/models.py
@@ -10,6 +10,10 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Task(models.Model):
@@ -53,3 +57,9 @@ class Task(models.Model):
             str: The title of the task
         """
         return self.title
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
