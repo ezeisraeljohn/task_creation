64e9d7381cfb573112da5a1048092cabf8889b44
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-11 10:23:10 +0100

    chore: Update URL paths in urls.py
    This commit updates the URL paths in the urls.py file. The 'index' path has been changed to 'dashboard' and the 'signup' path has been moved to 'accounts/signup'. This change ensures that the correct URLs are used for the dashboard and signup pages in the Task Creation App.

--- task_creation_app/urls.py
+++ task_creation_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import index, signup, members
+from .views import dashboard, signup, members
 
 
 urlpatterns = [
     path("accounts/login/", include("django.contrib.auth.urls"), name="login"),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("dashboard/", view=index, name="dashboard"),
+    path("dashboard/", view=dashboard, name="dashboard"),
     path("accounts/signup/", view=signup, name="signup"),
     path("members/", view=members, name="members"),
 ]
